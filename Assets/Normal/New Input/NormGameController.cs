6ab2c774e79de2e62b5f3eaf2a477949e05aa8b1
Lala-banners <lala.l.banno@hotmail.com>
2022-07-15 13:20:20 +1000

    more more testing - multiplayer works lol

--- Assets/Normal/New Input/NormGameController.cs
+++ Assets/Normal/New Input/NormGameController.cs
@@ -1,122 +1,127 @@
 using System.Collections;
+
 using UnityEngine;
+
 using TMPro;
+
 using Normal.Realtime.Examples;
 
 public class NormGameController : MonoBehaviour
 {
-      [SerializeField] float playTime = 50;
+	[SerializeField] float playTime = 50;
 
-    [Header("GameObject Binding")]
-    [SerializeField] CoinSpawner spawner;
-    [SerializeField] NormPlayerPC player;
+	[Header("GameObject Binding")] [SerializeField]
+	CoinSpawner spawner;
 
-    // UI
-	[Header("UI Binding")]
-    [SerializeField] GameObject gameView;
-    [SerializeField] GameObject resultView;
-    [SerializeField] TMP_Text timeText;
-    [SerializeField] TMP_Text coinText;
-    [SerializeField] TMP_Text resultCoinText;
+	[SerializeField] NormPlayerPC player;
 
-    // Internal Data
-    protected int coin;
-    protected float time;
-    protected bool isPlaying = false;
-    protected bool waitForNewCoin = false;
+	// UI
+	[Header("UI Binding")] [SerializeField]
+	GameObject gameView;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        SetupPlayer();
-        StartGame();
-    }
+	[SerializeField] GameObject resultView;
+	[SerializeField] TMP_Text timeText;
+	[SerializeField] TMP_Text coinText;
+	[SerializeField] TMP_Text resultCoinText;
 
-	void SetupPlayer()
+	// Internal Data
+	protected int coin;
+	protected float time;
+	protected bool isPlaying = false;
+	protected bool waitForNewCoin = false;
+
+	// Start is called before the first frame update
+	void Start()
 	{
-        player.onCollectCoin = () =>
-        {
-			if(isPlaying)
-			{
-                AddCoin();
-			}
-        };
+		//SetupPlayer();
+		StartGame();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	/*void SetupPlayer()
+	{
+		player.onCollectCoin = () =>
+		{
+			if(isPlaying)
+			{
+				AddCoin();
+			}
+		};
+	}*/
+
+	// Update is called once per frame
+	void Update()
+	{
 		if(isPlaying)
 		{
-            HandlePlayLogic();
-        }
-    }
+			HandlePlayLogic();
+		}
+	}
 
 	void HandlePlayLogic()
 	{
-        time -= Time.deltaTime;
+		time -= Time.deltaTime;
 		if(time < 0)
 		{
-            EndGame();
-            return;
+			EndGame();
+			return;
 		}
 
-        CheckAndRespawnCoin();
-        UpdateTimeValue();
+		CheckAndRespawnCoin();
+		UpdateTimeValue();
 	}
 
 	void Reset()
 	{
-        coin = 0;
+		coin = 0;
 		time = playTime + 0.3f;
-        waitForNewCoin = false;
-    }
+		waitForNewCoin = false;
+	}
 
 	public void StartGame()
 	{
-        Reset();
-        ShowGameView();
-        spawner.ClearCoins();
-        spawner.SpawnCoins();
+		Reset();
+		ShowGameView();
+		spawner.ClearCoins();
+		spawner.SpawnCoins();
 
-        isPlaying = true;
+		isPlaying = true;
 	}
 
-    public void EndGame()
-    {
-        isPlaying = false;
-        ShowResultView();
-    }
+	public void EndGame()
+	{
+		isPlaying = false;
+		ShowResultView();
+	}
 
 	#region Coin Logic
 
 	void CheckAndRespawnCoin()
 	{
 		//Debug.Log("Coin Count=" + spawner.GetCoinCount());
-		if (spawner.GetCoinCount() == 0 && waitForNewCoin == false)
+		if(spawner.GetCoinCount() == 0 && waitForNewCoin == false)
 		{
 			waitForNewCoin = true;
-            
-            StartCoroutine(LateSpawnCoins());
+
+			StartCoroutine(LateSpawnCoins());
 		}
 	}
 
-    IEnumerator LateSpawnCoins()
+	IEnumerator LateSpawnCoins()
 	{
-        yield return new WaitForSeconds(1.0f);
+		yield return new WaitForSeconds(1.0f);
 
 		//
-        if (isPlaying != false && spawner.GetCoinCount() == 0)
-        {
-            spawner.SpawnCoins();
-            waitForNewCoin = false;	// Reset the flag
-        }
-    }
+		if(isPlaying != false && spawner.GetCoinCount() == 0)
+		{
+			spawner.SpawnCoins();
+			waitForNewCoin = false; // Reset the flag
+		}
+	}
 
 	public void AddCoin()
 	{
-        coin++;
-        UpdateCoinValue();
+		coin++;
+		UpdateCoinValue();
 	}
 
 	#endregion
@@ -125,30 +130,29 @@ public class NormGameController : MonoBehaviour
 
 	public void ShowGameView()
 	{
-        UpdateTimeValue();
-        UpdateCoinValue();
-        gameView.SetActive(true);
-        resultView.SetActive(false);
+		UpdateTimeValue();
+		UpdateCoinValue();
+		gameView.SetActive(true);
+		resultView.SetActive(false);
 	}
 
-    public void ShowResultView()
-    {
-        resultCoinText.text = coin.ToString();
-
-        gameView.SetActive(false);
-        resultView.SetActive(true);
-    }
-
-    void UpdateTimeValue()
+	public void ShowResultView()
 	{
-        timeText.text = time.ToString("00");
+		resultCoinText.text = coin.ToString();
+
+		gameView.SetActive(false);
+		resultView.SetActive(true);
+	}
+
+	void UpdateTimeValue()
+	{
+		timeText.text = time.ToString("00");
 	}
 
 	void UpdateCoinValue()
 	{
-        coinText.text = coin.ToString();
-    }
+		coinText.text = coin.ToString();
+	}
 
 	#endregion
-
 }
--- Assets/Normal/New Input/NormMovement.cs
+++ Assets/Normal/New Input/NormMovement.cs
@@ -1,62 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
-public class NormMovement : MonoBehaviour
+namespace Normal.Realtime.Examples
 {
-    [SerializeField]
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
-    
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+	[RequireComponent(typeof(CharacterController))]
+	public class NormMovement : MonoBehaviour
+	{
+		[Header("Norm")] private RealtimeView _realtimeView;
+		private RealtimeTransform _realtimeTransform;
 
-    private Vector2 moveInput = Vector2.zero;
-    private bool jumped = false;
+		[SerializeField] private float playerSpeed = 2.0f;
+		private float jumpHeight = 1.0f;
+		private float gravityValue = -9.81f;
 
-    private void Start()
-    {
-        controller = gameObject.GetComponent<CharacterController>();
-    }
+		private CharacterController controller;
+		private Vector3 playerVelocity;
+		private bool groundedPlayer;
 
-    void Update()
-    {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+		private Vector2 moveInput = Vector2.zero;
+		private bool jumped = false;
 
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move * Time.deltaTime * playerSpeed);
+		private void Awake()
+		{
+			_realtimeView = GetComponent<RealtimeView>();
+			_realtimeTransform = GetComponent<RealtimeTransform>();
+		}
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+		private void Start()
+		{
+			controller = gameObject.GetComponent<CharacterController>();
+		}
 
-        // Changes the height position of the player..
-        if (jumped && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
+		void Update()
+		{
+			// If this CubePlayer prefab is not owned by this client, bail.
+			if(!_realtimeView.isOwnedLocallySelf)
+				return;
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
+			// Make sure we own the transform so that RealtimeTransform knows to use this client's transform to synchronize remote clients.
+			_realtimeTransform.RequestOwnership();
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
 
-    public void OnJumping(InputAction.CallbackContext context)
-    {
-        jumped = context.ReadValue<bool>();
-        jumped = context.action.triggered;
-    }
+			groundedPlayer = controller.isGrounded;
+			if(groundedPlayer && playerVelocity.y < 0)
+			{
+				playerVelocity.y = 0f;
+			}
+
+			Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+			controller.Move(move * Time.deltaTime * playerSpeed);
+
+			if(move != Vector3.zero)
+			{
+				gameObject.transform.forward = move;
+			}
+
+			// Changes the height position of the player..
+			if(jumped && groundedPlayer)
+			{
+				playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+			}
+
+			playerVelocity.y += gravityValue * Time.deltaTime;
+			controller.Move(playerVelocity * Time.deltaTime);
+		}
+
+		public void OnMove(InputAction.CallbackContext context)
+		{
+			moveInput = context.ReadValue<Vector2>();
+		}
+
+		public void OnJumping(InputAction.CallbackContext context)
+		{
+			jumped = context.ReadValue<bool>();
+			jumped = context.action.triggered;
+		}
+	}
 }
