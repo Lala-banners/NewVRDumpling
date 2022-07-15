157228b6e4514681887e560ec682ee9083d48bb7
Lala-banners <lala.l.banno@hotmail.com>
2022-07-15 10:55:59 +1000

    random

---
+++ Assets/Stylized Astronaut/Character/PlayerMovement.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private PlayerControls playerControls;
+    private InputAction move;
+    
+    private Rigidbody rb;
+    [SerializeField]
+    private float movementForce = 1f;
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField]
+    private float maxSpeed = 5f;
+    private Vector3 forceDirection = Vector3.zero;
+
+    [SerializeField]
+    private Camera playerCamera;
+    private Animator animator;
+    
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+
+        rb = GetComponent<Rigidbody>();
+
+        if(rb == null)
+        {
+            Debug.LogError("There is no Rigidbody in scene!");
+        }
+        
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Player_Map.Jump.started += DoJump;
+        move = playerControls.Player_Map.Movement;
+        playerControls.Player_Map.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Player_Map.Jump.started -= DoJump;
+        playerControls.Player_Map.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        forceDirection += move.ReadValue<Vector2>().x * GetCameraRight(playerCamera) * movementForce;
+        forceDirection += move.ReadValue<Vector2>().y * GetCameraForward(playerCamera) * movementForce;
+
+        rb.AddForce(forceDirection, ForceMode.Impulse);
+        forceDirection = Vector3.zero;
+
+        if (rb.velocity.y < 0f)
+            rb.velocity -= Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
+
+        Vector3 horizontalVelocity = rb.velocity;
+        horizontalVelocity.y = 0;
+        if (horizontalVelocity.sqrMagnitude > maxSpeed * maxSpeed)
+            rb.velocity = horizontalVelocity.normalized * maxSpeed + Vector3.up * rb.velocity.y;
+
+        LookAt();
+    }
+
+    private void LookAt()
+    {
+        Vector3 direction = rb.velocity;
+        direction.y = 0f;
+
+        if (move.ReadValue<Vector2>().sqrMagnitude > 0.1f && direction.sqrMagnitude > 0.1f)
+            this.rb.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        else
+            rb.angularVelocity = Vector3.zero;
+    }
+
+    private Vector3 GetCameraForward(Camera playerCamera)
+    {
+        Vector3 forward = playerCamera.transform.forward;
+        forward.y = 0;
+        return forward.normalized;
+    }
+
+    private Vector3 GetCameraRight(Camera playerCamera)
+    {
+        Vector3 right = playerCamera.transform.right;
+        right.y = 0;
+        return right.normalized;
+    }
+
+    private void DoJump(InputAction.CallbackContext obj)
+    {
+        if(IsGrounded())
+        {
+            forceDirection += Vector3.up * jumpForce;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(this.transform.position + Vector3.up * 0.25f, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, 0.3f))
+            return true;
+        else
+            return false; 
+    }
+
+    /*private void DoAttack(InputAction.CallbackContext obj)
+    {
+        animator.SetTrigger("Jump");
+    }*/
+}
