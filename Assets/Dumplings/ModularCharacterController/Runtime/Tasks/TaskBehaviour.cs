655535a2fe45f7301e799d2d1b338eb38fee5527
Kieran <kieranc.inbox@gmail.com>
2022-07-17 03:50:14 +1000

    Calling it a night, Task Behavior Conversion to MCC

---
+++ Assets/Dumplings/ModularCharacterController/Runtime/Tasks/TaskBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using ModularCharacterController;
+namespace Dumplings.ModularCharacterController.Runtime.Tasks
+{
+    public class TaskBehaviour : ModularBehaviour
+    {
+
+        protected override void OnProcess(UpdatePhase _phase)
+        {
+            
+        }
+    }
+}
